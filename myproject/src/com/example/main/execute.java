2f0510b2648a99379858c7e77e398f19377be32a
MYPC <MYPC@LAPTOP-5MQAU3TL>
2021-02-18 19:40:14 +0530

    Added sysout in main method

--- myproject/src/com/example/main/execute.java
+++ myproject/src/com/example/main/execute.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 public class execute {
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		System.out.println("Iam in Main Method");
 		ReadingData.updateEmployee();
 		
 	}
