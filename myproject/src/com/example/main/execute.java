1baf22a672d9e3349023fd7d637e9a960685377d
MYPC <MYPC@LAPTOP-5MQAU3TL>
2021-02-18 19:24:54 +0530

    initial commit

---
+++ myproject/src/com/example/main/execute.java
@@ -0,0 +1,11 @@
+package com.example.main;
+
+import java.io.IOException;
+
+public class execute {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		ReadingData.updateEmployee();
+		
+	}
+
+}
